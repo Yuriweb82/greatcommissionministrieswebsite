6ccae4ec58cfb6578a0d376719ebbca3154904d2
YuriTech82 <shinjungtee@gmail.com>
2023-09-06 22:37:41 +0300

    finest update

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ menu.onclick = () => {
 };
 
 
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_khnao4l';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
